b2ed8cfc8c82410d79dc8f393b4b39a8a77d2cfa
Vijay Patil <vijay4patil12@gmail.com>
2025-09-16 15:03:43 +0530

    Updated

---
+++ src/main/java/com/project/smartEMS/SmartEmsApplication.java
@@ -0,0 +1,13 @@
+package com.project.smartEMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/smartEMS/SmartEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smartEMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
