727c2f6f8388388db3181c0fa2f83a1af5a1dfa7
Vijay Patil <vijay4patil12@gmail.com>
2025-09-21 04:55:38 +0530

    Updated

--- src/main/java/com/project/smartEMS/entity/User.java
+++ src/main/java/com/project/smartEMS/entity/User.java
@@ -25,4 +25,31 @@ public class User {
 
     @Column(nullable = false)
     private String role; // ADMIN, HR, EMPLOYEE
+
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
